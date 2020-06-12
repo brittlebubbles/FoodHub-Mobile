8a9bac0b9a50dacc81816049176336a71165d3d2
Adom Sackey <adom.466@gmail.com>
2020-06-12 21:47:20 +0000

    adds: RecipieDetails screen

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomTabNavigation from "./BottomTabNavigation";
-
+import RecipeDetails from "../screens/RecipeDetails";
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
@@ -15,6 +15,7 @@ export default function StackNavigator() {
           headerShown: false,
         }}
       />
+      <Stack.Screen name="RecipeDetails" component={RecipeDetails} />
     </Stack.Navigator>
   );
 }
